30dc35176c2ae6fa8ea20e3d8d32b2ae7a4643cb
spy100 <wdesigner2010@gmail.com>
2020-07-28 05:02:59 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 Create a database or a table 
 
-CREATE TABLE vAntiBot (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    key TEXT NOT NULL,
-    hash VARCHAR(255) NOT NULL,
-)
+    CREATE TABLE vAntibot (
+      id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+      vkey VARCHAR(255) NOT NULL,
+      vhash VARCHAR(255) NOT NULL
+    );
